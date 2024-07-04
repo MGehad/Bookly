3ff784d50f32e18c4b8c869df5f9abf5887893ea
MGehad <134580414+MGehad@users.noreply.github.com>
2024-07-04 16:06:40 +0300

    Created Bookly app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: SplashView(),
+    );
+  }
+}
